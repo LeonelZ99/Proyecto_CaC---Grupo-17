936a79c55de99859773403a8e07e01842f56a315
Leonel <104234656+LeonelZ99@users.noreply.github.com>
2024-06-27 16:28:16 -0300

    Update dump-arana.sql

--- back-end/bd/dump-arana.sql
+++ back-end/bd/dump-arana.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.36, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
--- Host: localhost    Database: paciencia_arana
+-- Host: 127.0.0.1    Database: web_tejidos
 -- ------------------------------------------------------
--- Server version	8.0.36
+-- Server version	8.0.37
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -28,7 +28,7 @@ CREATE TABLE `autor` (
   `apellido` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `nacionalidad` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   PRIMARY KEY (`id_autor`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='autores de las prendas publicadas';
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='autores de las prendas publicadas';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,6 +37,7 @@ CREATE TABLE `autor` (
 
 LOCK TABLES `autor` WRITE;
 /*!40000 ALTER TABLE `autor` DISABLE KEYS */;
+INSERT INTO `autor` VALUES (1,'Juan','Perez','Argentina'),(2,'Maria','Gomez','Mexico'),(3,'Carlos','Rodriguez','España');
 /*!40000 ALTER TABLE `autor` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -62,6 +63,30 @@ LOCK TABLES `categoria` WRITE;
 /*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
 INSERT INTO `categoria` VALUES (1,'gorros'),(2,'pullover');
 /*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `paises`
+--
+
+DROP TABLE IF EXISTS `paises`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `paises` (
+  `idPaises` int NOT NULL AUTO_INCREMENT,
+  `nombrePais` varchar(100) NOT NULL,
+  PRIMARY KEY (`idPaises`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `paises`
+--
+
+LOCK TABLES `paises` WRITE;
+/*!40000 ALTER TABLE `paises` DISABLE KEYS */;
+INSERT INTO `paises` VALUES (1,'Argentina'),(2,'Brazil'),(3,'Chile'),(4,'Peru'),(5,'Colombia'),(6,'Uruguay');
+/*!40000 ALTER TABLE `paises` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -91,12 +116,40 @@ CREATE TABLE `prenda` (
 
 LOCK TABLES `prenda` WRITE;
 /*!40000 ALTER TABLE `prenda` DISABLE KEYS */;
+INSERT INTO `prenda` VALUES (1,'Gorro de lana','Gorro de lana hecho a mano',1,1),(2,'Pullover de algodon','Pullover comodo y ligero',2,2),(3,'Gorro de invierno','Gorro con forro polar',1,1),(4,'Pullover de lana','Pullover grueso para invierno',2,3);
 /*!40000 ALTER TABLE `prenda` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Dumping routines for database 'paciencia_arana'
+-- Table structure for table `usuarios`
 --
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `idUsuario` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `idPais_Usuarios` int DEFAULT NULL,
+  `clave` varchar(250) NOT NULL,
+  `fechaNacimiento` date NOT NULL,
+  PRIMARY KEY (`idUsuario`),
+  KEY `idPais_Usuarios` (`idPais_Usuarios`),
+  CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`idPais_Usuarios`) REFERENCES `paises` (`idPaises`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'Leonel','Zapata','leonel@hotmail.com',1,'contra1234','1999-11-09'),(2,'Maria','DaSilva','maria.gomez@example.com',2,'securepass','1985-07-30'),(3,'Carlos','Lopez','carlos.lopez@example.com',3,'mypassword','1992-11-23'),(4,'Ana','Martinez','ana.martinez@example.com',4,'anotherpass','1988-05-10'),(5,'Luis','Rodriguez','luis.rodriguez@example.com',5,'newpassword','1995-03-05'),(6,'Jhon','Perez','jhon.perez@example.com',1,'pa123','1990-01-15'),(7,'Carlos','DaSilva','c.gomez@example.com',1,'sepass','1985-07-30'),(8,'Ana','Lopez','ana.lopez@example.com',2,'mypassword','1992-11-23'),(9,'Luis','Martinez','luis.martinez@example.com',4,'otrapass','1988-05-10'),(10,'Luisa','Rodriguez','luisa.rodriguez@example.com',4,'nuevapass','1995-03-05');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -107,4 +160,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-06-24 13:29:30
+-- Dump completed on 2024-06-27 16:27:10
